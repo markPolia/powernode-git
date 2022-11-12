159f3778953478112894dbe50a101b75549ba2ca
Augenstern_ <2415286134@qq.com>
2022-11-12 23:32:46 +0800

    ssm milestone version template, which with generator plug

--- src/main/java/com/bjpowernode/web/controller/IndexAction.java
+++ src/main/java/com/bjpowernode/web/controller/IndexAction.java
@@ -1,5 +1,7 @@
 package com.bjpowernode.web.controller;
 
+import com.bjpowernode.web.service.IndexService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,13 +9,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/work")
 public class IndexAction {
+    @Resource(type = IndexService.class)
+    IndexService service;
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public void indexAction() {
         System.out.println("访问index");
     }
 
     @RequestMapping(value = "/indexJsp", method = RequestMethod.GET)
+    @SuppressWarnings("all")
     public String indexJsp() {
         return "redirect:/index.jsp";
+    }
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public void hi() {
+        service.doSome();
     }
 }
--- src/main/java/com/bjpowernode/web/util/Util.java
+++ src/main/java/com/bjpowernode/web/util/Util.java
@@ -2,6 +2,6 @@ package com.bjpowernode.web.util;
 
 public class Util {
     public void doElse() {
-        
+
     }
 }
